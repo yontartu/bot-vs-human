b084dea1da87bfd3c16d20d66cb658785b3c126e
yontartu <jfg47@georgetown.edu>
2019-08-27 23:21:30 -0400

    move generate hashtags counts function to d04_visualizations

--- src/d01_data/data_processing.py
+++ src/d01_data/data_processing.py
@@ -142,31 +142,7 @@ def trim_right_trolls_csv(orig_filepath, new_filepath):
     df.to_csv(new_filepath, index=False, date_format='%Y-%m-%d %H:%M:%S')
     print('  Saved', new_filepath, 'with shape', df.shape)
 
-def generate_hashtags_counts(df, new_filepath):
-    """
-    Generates a dataframe of all hashtags and corresponding counts,
-    ordered by most to least instances.
-    
-    Parameters
-    ----------
-        df : a dataframe containing a column for 'hashtags' (a list of hashtags)
-             and another column for 'hashtags_count' (the number of hashtags)
-        new_filepath : the name and path to save the dataframe with hashtag counts
-        
-    Returns
-    -------
-        None. Prints a progress report and the filepath to the new file.
-    """
-    print('Processing all hashtags into one list...')
-    all_hashtags = df[df.hashtags_count > 0].hashtags.sum()
-    print('...done.')
-    print('List of all hashtags has length', len(all_hashtags))
-    top_hashtags = pd.DataFrame.from_dict(Counter(all_hashtags), orient='index').reset_index()
-    top_hashtags.columns = ['hashtag', 'counts']
-    top_hashtags.sort_values('counts', ascending=False, inplace=True)
-    top_hashtags = top_hashtags.reset_index(drop=True)
-    top_hashtags.to_csv(new_filepath, index=False)
-    print('  Saved', new_filepath, 'with shape', top_hashtags.shape)
+
 
 
 
