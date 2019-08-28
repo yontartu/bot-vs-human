efac0d493476bd7d924f997a5632a6ffa6d69b76
yontartu <jfg47@georgetown.edu>
2019-08-27 23:22:50 -0400

    add first 15 twint command line queries

---
+++ collect_verified_tweets.sh
@@ -0,0 +1,17 @@
+# twint -s "#MAGA" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 7000 --csv -o data/01_raw/verified_tweets/twint_query_MAGA.csv --lang en
+# twint -s "#top" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 6500 --csv -o data/01_raw/verified_tweets/twint_query_top.csv --lang en
+# twint -s "#mar" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 4700 --csv -o data/01_raw/verified_tweets/twint_query_mar.csv --lang en
+# twint -s "#PJNET" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 4700 --csv -o data/01_raw/verified_tweets/twint_query_PJNET.csv --lang en
+# twint -s "#FAKENEWS" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 4300 --csv -o data/01_raw/verified_tweets/twint_query_FAKENEWS.csv --lang en
+# twint -s "#tcot" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 4000 --csv -o data/01_raw/verified_tweets/twint_query_tcot.csv --lang en
+# twint -s "#IslamKills" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 3900 --csv -o data/01_raw/verified_tweets/twint_query_IslamKills.csv --lang en
+twint -s "#topl" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 3800 --csv -o data/01_raw/verified_tweets/twint_query_topl.csv --lang en
+# twint -s "#amb" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2800 --csv -o data/01_raw/verified_tweets/twint_query_amb.csv --lang en
+twint -s "#DemnDebate" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2400 --csv -o data/01_raw/verified_tweets/twint_query_DemnDebate.csv --lang en
+# twint -s "#GOPDebate" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2400 --csv -o data/01_raw/verified_tweets/twint_query_GOPDebate.csv --lang en
+# twint -s "#th" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2200 --csv -o data/01_raw/verified_tweets/twint_query_th.csv --lang en
+# twint -s "#may" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2100 --csv -o data/01_raw/verified_tweets/twint_query_may.csv --lang en
+# twint -s "#DemDebate" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2100 --csv -o data/01_raw/verified_tweets/twint_query_DemDebate.csv --lang en
+# twint -s "#VegasGOPDebate" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_VegasGOPDebate.csv --lang en
+# twint -s "#WakeUpAmerica" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_WakeUpAmerica.csv --lang en
+
