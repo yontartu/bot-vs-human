536cf0f31ce407b5d4aeaee8f99b4958cc4da4e2
yontartu <jfg47@georgetown.edu>
2019-08-28 13:08:53 -0400

    finished collecting top 50 hashtags with twint

--- collect_verified_tweets.sh
+++ collect_verified_tweets.sh
@@ -1,3 +1,5 @@
+### TOP 50 hashtags (with counts over 1000) ###
+
 # twint -s "#MAGA" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 7000 --csv -o data/01_raw/verified_tweets/twint_query_MAGA.csv --lang en
 # twint -s "#top" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 6500 --csv -o data/01_raw/verified_tweets/twint_query_top.csv --lang en
 # twint -s "#mar" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 4700 --csv -o data/01_raw/verified_tweets/twint_query_mar.csv --lang en
@@ -14,4 +16,48 @@ twint -s "#DemnDebate" --verified --hashtags --since 2015-01-01 --until 2018-01-
 # twint -s "#DemDebate" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2100 --csv -o data/01_raw/verified_tweets/twint_query_DemDebate.csv --lang en
 # twint -s "#VegasGOPDebate" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_VegasGOPDebate.csv --lang en
 # twint -s "#WakeUpAmerica" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_WakeUpAmerica.csv --lang en
+# twint -s "#Brussels" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_Brussels.csv --lang en
+# twint -s "#2A" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_2A.csv --lang en
+# twint -s "#jud" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_jud.csv --lang en
+# twint -s "#ccot" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 2000 --csv -o data/01_raw/verified_tweets/twint_query_ccot.csv --lang en
+# twint -s "#TrumpForPresident" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_TrumpForPresident.csv --lang en
+# twint -s "#StopIslam" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_StopIslam.csv --lang en
+# twint -s "#USFA" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_USFA.csv --lang en
+# twint -s "#imo" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_imo.csv --lang en
+# twint -s "#Trump" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_Trump.csv --lang en
+# twint -s "#TrumpTrain" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_TrumpTrain.csv --lang en
+# twint -s "#TrumpBecause" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_TrumpBecause.csv --lang en
+twint -s "#vvmar" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_vvmar.csv --lang en
+# twint -s "#marv" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_marv.csv --lang en
+# twint -s "#Trump2016" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_Trump2016.csv --lang en
+# twint -s "#ells" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_ells.csv --lang en
+# twint -s "#ella" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_ella.csv --lang en
+# twint -s "#law" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_law.csv --lang en
+# twint -s "#laww" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_laww.csv --lang en
+twint -s "#dannis" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_dannis.csv --lang en
+# twint -s "#danita" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_danita.csv --lang en
+# twint -s "#job" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_job.csv --lang en
+# twint -s "#Prayers4California" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1600 --csv -o data/01_raw/verified_tweets/twint_query_Prayers4California.csv --lang en
+# twint -s "#jobb" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_jobb.csv --lang en
+# twint -s "#just" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_just.csv --lang en
+# twint -s "#iva" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_iva.csv --lang en
+# twint -s "#iv" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_iv.csv --lang en
+twint -s "#eevan" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_eevan.csv --lang en
+# twint -s "#evas" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_evas.csv --lang en
+# twint -s "#MakeAmericaGreatAgain" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_MakeAmericaGreatAgain.csv --lang en
+# twint -s "#arre" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_arre.csv --lang en
+# twint -s "#ara" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_ara.csv --lang en
+# twint -s "#Charlottesville" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_Charlottesville.csv --lang en
+# twint -s "#jan" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_jan.csv --lang en
 
+
+### Top hashtags with capitalized letters ###
+# twint -s "#StopTheGOP" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_StopTheGOP.csv --lang en
+# twint -s "#Obama" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_Obama.csv --lang en
+# twint -s "#CruzCrew" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_CruzCrew.csv --lang en
+twint -s "#ISIS" --verified --hashtags --since 2015-01-01 --until 2018-01-01 --filter-retweets --limit 1200 --csv -o data/01_raw/verified_tweets/twint_query_ISIS.csv --lang en
+
+
+
+
+### Collect ~100k tweets not based on a specific hashtag ###
